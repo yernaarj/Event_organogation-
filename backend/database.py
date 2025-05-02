85794e290065bcf3a3c0a1d11670283d95efa4ce
yernaarj <147658681+yernaarj@users.noreply.github.com>
2025-05-02 13:59:37 +0500

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"  # ← Это путь к файлу БД
+# Подключение к БД по URL из окружения,
+# на Render.com он задаётся автоматически.
+# Для локалки отступает sqlite:///./event.db
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./event.db"
+)
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    # check_same_thread нужен только для sqlite
+    connect_args={"check_same_thread": False}
+    if DATABASE_URL.startswith("sqlite")
+    else {}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
