034170cb2c0478e3905334773f307f5e2d2704f8
yernaarj <147658681+yernaarj@users.noreply.github.com>
2025-05-02 21:58:42 +0500

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -18,7 +18,7 @@ function LoginPage() {
       return;
     }
     try {
-      const res = await fetch(`${API}/login`, {
+      const res = await fetch(`/api/login`, {
         method: 'POST',
         headers: { 'Content-Type':'application/json' },
         body: JSON.stringify(formData)
